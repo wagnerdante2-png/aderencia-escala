9980c9d3f960b5139d5f1d2d8f2ffd0214b15f38
wagnerdante2-png <wagnerdante2@gmail.com>
2026-08-26 09:47:57 -0300

    fix: restaura cadastro dinâmico ao reabrir aplicação

--- bootstrap.js
+++ bootstrap.js
@@ -20,5 +20,6 @@ document.write('<script src="divergence-dashboard.js"><\/script>');
 document.write('<script src="divergence-capture-rc20.js"><\/script>');
 document.write('<script src="layout-fixes-rc19.js"><\/script>');
 document.write('<script src="store-management.js"><\/script>');
+document.addEventListener('DOMContentLoaded',()=>{const r=window.ADERENCIA_STORE_REGISTRY;if(r)r.importRegistry(r.load())});
 loadBatch();
 })();
